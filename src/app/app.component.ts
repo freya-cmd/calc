761e85b46b0607e36955619ccd271013b4f0a3d2
Nathaniel Betancourt <nathanielbetancourt@hotmail.com>
2022-02-08 09:22:56 -0500

    add all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calc';
+  clicked = false;
+
+  additionForm!: FormGroup;
+
+  constructor (private fb: FormBuilder) { 
+
+  }
+
+  ngOnInit() {
+    this.initializeForm();
+  }
+  initializeForm(): void {
+    this.additionForm = this.fb.group({
+      number1: '',
+      number2: '',
+    })
+  }
+  onSubmit(): void {
+    console.log(this.additionForm);
+  }
+
+
+  handleClick() {
+    this.clicked = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
